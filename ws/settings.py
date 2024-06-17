12eb4607a2fc255008e345edd5f29c32084f46d6
Sai Kumar <dagudusai@gmail.com>
2024-06-17 12:34:43 +0530

    removed cloud sql

--- ws/settings.py
+++ ws/settings.py
@@ -77,11 +77,26 @@ ASGI_APPLICATION = 'ws.asgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# from decouple import config
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': config('DB_HOST'),
+#             'PORT': config('DB_PORT'),
+#         }
+#     }
 
 CHANNEL_LAYERS = {
     'default':{
