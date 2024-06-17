e178e00397dd8203a7cb2218d03726fe652e7846
Sai Kumar <dagudusai@gmail.com>
2024-06-17 12:49:08 +0530

    requirements updated

--- ws/settings.py
+++ ws/settings.py
@@ -89,7 +89,7 @@ from decouple import config
 
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
