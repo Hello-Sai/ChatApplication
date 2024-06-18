ec8c3021d878c9c7f46309de1ef45117c64a4cb1
Sai Kumar <dagudusai@gmail.com>
2024-06-18 18:43:10 +0530

    settings changed

--- ws/settings.py
+++ ws/settings.py
@@ -89,7 +89,7 @@ from decouple import config
 
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
